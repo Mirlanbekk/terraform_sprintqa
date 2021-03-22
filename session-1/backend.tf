3f2e5cfc63ab049c3e6995947ae98a2acb15006f
marzhansuluu <marzhansuluu@gmail.com>
2021-03-22 16:45:28 -0700

    added jenkins provisioner file

--- session-1/backend.tf
+++ session-1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformback0101"
+    bucket = "elasticbeanstalk-us-east-1-749327794284"
     key    = "tfstate/webserver.tfstate"
     region = "us-east-1"
   }

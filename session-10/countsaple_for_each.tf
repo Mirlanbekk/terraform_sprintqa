52587e6eda2ef0778769c0f80bddb4488f4641cc
marzhansuluu <marzhansuluu@gmail.com>
2021-03-28 12:54:02 -0700

    for loop count for each

---
+++ session-10/countsaple_for_each.tf
@@ -0,0 +1,25 @@
+# resource "aws_vpc" "my_vpc" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
+#   tags = {
+#     Name = "my_vpc"
+#   }
+# }
+# variable "cidr_block_public" {
+#   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+# }
+# variable "cidr_block_private" {
+#   default = ["10.0.4.0/24", "10.0.5.0/24"]
+# }
+# resource "aws_subnet" "public_subnet" {
+#   for_each = toset(var.cidr_block_public)
+#   vpc_id = aws_vpc.my_vpc.id
+#   cidr_block = each.value
+#   map_public_ip_on_launch = true
+# }
+# resource "aws_subnet" "private_subnet" {
+#   for_each = toset(var.cidr_block_private)
+#   vpc_id = aws_vpc.my_vpc.id
+#   cidr_block = each.value
+#   map_public_ip_on_launch = false
+# }
---
+++ session-10/instance.tf
@@ -0,0 +1,22 @@
+# provider "aws" {
+#     region = "us-east-1"
+# }
+# resource "aws_instance" "web" {
+#   for_each = {
+#       dev = "t2.micro",
+#       prod = "t2.large"
+#   }
+#   ami                    = "ami-038f1ca1bd58a5790"
+#   instance_type          = each.value
+#   tags = {
+#     # Name = "My_web ${count.index}"
+#       Name = "My_web ${each.key}"
+#   }
+# }
+
+# output "instance_ip" {
+
+#     # value = aws_instance.web[*].public_ip
+#     value = [for instance in aws_instance.web: instance.public_ip]
+
+# }
